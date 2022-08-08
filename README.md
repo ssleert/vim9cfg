5db4cf729914865b352b4615fbdcaee0a9ef5da8
sfome <semenilichevsky@gmail.com>
2022-08-08 06:22:06 +0300

    Init: init

---
+++ README.md
@@ -0,0 +1,2 @@
+# My fully native vim9 config
+
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+installPlug() {
+  git clone $1
+}
+
+rm -rvf ~/.vim
+cp src/vimrc ~/.vim/vimrc
+mkdir -p ~/.vim/pack/default/start
+cd ~/.vim/pack/default/start
+
+# install plugins
+# light line plugin
+installPlug "https://github.com/itchyny/lightline.vim.git" &
+
+# plugins for auto pairs quotes
+installPlug "https://github.com/jiangmiao/auto-pairs.git" &
+
+# simple autocomplete plugin
+installPlug "https://github.com/ervandew/supertab.git" &
+
+# nim lang support for vim
+installPlug "https://github.com/zah/nim.vim.git" &
+
+# vscode theme for vim
+installPlug "https://github.com/tomasiser/vim-code-dark.git" &
+
+# floating term for vim
+installPlug "https://github.com/voldikss/vim-floaterm.git" &
+
+# async lint engine (recommended)
+installPlug "https://github.com/dense-analysis/ale.git" &
+
