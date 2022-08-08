732ab6e4c979d45638fb9b3a6c67c1251ef84256
sfome <semenilichevsky@gmail.com>
2022-08-08 09:53:02 +0300

    Update: added config builder

--- setup.sh
+++ setup.sh
@@ -1,35 +1,105 @@
 #!/usr/bin/env sh
 
 installPlug() {
-  git clone $1
+  git clone "https://github.com/$1"
+}
+
+removeFile() {
+  rm -rfv $1
 }
 
 rm -rvf ~/.vim
 mkdir -p ~/.vim/pack/default/start
 cp src/vimrc ~/.vim/vimrc
+
+# change dir to plugins dir
 cd ~/.vim/pack/default/start
+
 
 # install plugins
 # light line plugin
-installPlug "https://github.com/itchyny/lightline.vim.git" &
+installPlug "itchyny/lightline.vim.git"
 
 # plugins for auto pairs quotes
-installPlug "https://github.com/jiangmiao/auto-pairs.git" &
+installPlug "jiangmiao/auto-pairs.git"
 
 # simple autocomplete plugin
-installPlug "https://github.com/ervandew/supertab.git" &
+installPlug "ervandew/supertab.git"
 
 # nim lang support for vim
-installPlug "https://github.com/zah/nim.vim.git" &
+# installPlug "https://github.com/zah/nim.vim.git" &
+# deprecated shit)
+
+# super puper duper nim support
+# installPlug "https://github.com/wsdjeg/vim-nim.git" &
+installPlug "ssleert/vin.git"
 
 # vscode theme for vim
-installPlug "https://github.com/tomasiser/vim-code-dark.git" &
+installPlug "tomasiser/vim-code-dark.git"
 
 # floating term for vim
-installPlug "https://github.com/voldikss/vim-floaterm.git" &
+installPlug "voldikss/vim-floaterm.git"
 
 # async lint engine (recommended)
-installPlug "https://github.com/dense-analysis/ale.git" &
+installPlug "dense-analysis/ale.git"
 
 
+# remove unused files
+# remove unused from lightline
+removeFile "lightline.vim/colorscheme.md"
+removeFile "lightline.vim/LICENSE"
+removeFile "lightline.vim/README.md"
+removeFile "lightline.vim/test/"
+removeFile "lightline.vim/doc/"
+removeFile "lightline.vim/.git/"
 
+# remove unused from auto-pairs
+removeFile "auto-pairs/doc/"
+removeFile "auto-pairs/README.md"
+removeFile "auto-pairs/.git/"
+
+# remove unused from supertab
+removeFile "supertab/doc/"
+removeFile "supertab/README.rst"
+removeFile "supertab/Makefile"
+removeFile "supertab/.git/"
+
+# remove unused from vim-nim
+# no unused)
+
+# remove unused from vim-code-dark
+removeFile "vim-code-dark/LICENSE.md"
+removeFile "vim-code-dark/README.md"
+removeFile "vim-code-dark/base16/"
+removeFile "vim-code-dark/.git/"
+
+# remove unused from vim-floaterm
+removeFile "vim-floaterm/test/"
+removeFile "vim-floaterm/README.md"
+removeFile "vim-floaterm/LICENSE"
+removeFile "vim-floaterm/doc/"
+removeFile "vim-floaterm/.git/"
+
+# remove unused from ale
+removeFile "ale/test/"
+removeFile "ale/rplugin/"
+removeFile "ale/LICENSE"
+removeFile "ale/README.md"
+removeFile "ale/run-tests"
+removeFile "ale/run-tests.bat"
+removeFile "ale/Dockerfile"
+removeFile "ale/supported-tools.md"
+removeFile "ale/test/"
+removeFile "ale/doc/"
+removeFile "ale/.git/"
+
+# template
+# removeFile ""
+
+
+echo ""
+echo ""
+echo "    Config builded!!!"
+echo "    have a nice day)"
+echo ""
+echo ""
--- src/pack/default/start/vim-floaterm/autoload/floaterm/edita/setup.vim
+++
@@ -1,5 +0,0 @@
-function! floaterm#edita#setup#EDITOR() abort
-  return has('nvim')
-        \ ? floaterm#edita#neovim#client#EDITOR()
-        \ : floaterm#edita#vim#client#EDITOR()
-endfunction
