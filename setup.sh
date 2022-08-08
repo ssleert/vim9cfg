bdb7e16bacd24c8b2b2cf0b2722100864ee99573
sfome <semenilichevsky@gmail.com>
2022-08-08 10:46:30 +0300

    Update: fixed pum and popup menu

--- setup.sh
+++ setup.sh
@@ -18,13 +18,13 @@ cd ~/.vim/pack/default/start
 
 # install plugins
 # light line plugin
-installPlug "itchyny/lightline.vim.git"
+installPlug "itchyny/lightline.vim.git" &
 
 # plugins for auto pairs quotes
-installPlug "jiangmiao/auto-pairs.git"
+installPlug "jiangmiao/auto-pairs.git" &
 
 # simple autocomplete plugin
-installPlug "ervandew/supertab.git"
+installPlug "ervandew/supertab.git" &
 
 # nim lang support for vim
 # installPlug "https://github.com/zah/nim.vim.git" &
@@ -32,74 +32,90 @@ installPlug "ervandew/supertab.git"
 
 # super puper duper nim support
 # installPlug "https://github.com/wsdjeg/vim-nim.git" &
-installPlug "ssleert/vin.git"
+installPlug "ssleert/vin.git" &
 
 # vscode theme for vim
-installPlug "tomasiser/vim-code-dark.git"
+installPlug "tomasiser/vim-code-dark.git" &
 
 # floating term for vim
-installPlug "voldikss/vim-floaterm.git"
+installPlug "voldikss/vim-floaterm.git" &
 
 # async lint engine (recommended)
-installPlug "dense-analysis/ale.git"
+installPlug "dense-analysis/ale.git" &
 
+sleep 15
 
 # remove unused files
 # remove unused from lightline
-removeFile "lightline.vim/colorscheme.md"
-removeFile "lightline.vim/LICENSE"
-removeFile "lightline.vim/README.md"
-removeFile "lightline.vim/test/"
-removeFile "lightline.vim/doc/"
-removeFile "lightline.vim/.git/"
+removeFile "lightline.vim/colorscheme.md" &
+removeFile "lightline.vim/LICENSE" &
+removeFile "lightline.vim/README.md" &
+removeFile "lightline.vim/test/" &
+removeFile "lightline.vim/doc/" &
+removeFile "lightline.vim/.git/" &
+removeFile "lightline.vim/.gitignore/" &
+removeFile "lightline.vim/.github/" &
 
 # remove unused from auto-pairs
-removeFile "auto-pairs/doc/"
-removeFile "auto-pairs/README.md"
-removeFile "auto-pairs/.git/"
+removeFile "auto-pairs/doc/" &
+removeFile "auto-pairs/README.md" &
+removeFile "auto-pairs/.git/" &
+removeFile "auto-pairs/.gitignore/" &
 
 # remove unused from supertab
-removeFile "supertab/doc/"
-removeFile "supertab/README.rst"
-removeFile "supertab/Makefile"
-removeFile "supertab/.git/"
+removeFile "supertab/doc/" &
+removeFile "supertab/README.rst" &
+removeFile "supertab/Makefile" &
+removeFile "supertab/.git/" &
+removeFile "supertab/.gitignore/" &
 
-# remove unused from vim-nim
-# no unused)
+# remove unused from vin
+removeFile "vin/.git/" &
 
 # remove unused from vim-code-dark
-removeFile "vim-code-dark/LICENSE.md"
-removeFile "vim-code-dark/README.md"
-removeFile "vim-code-dark/base16/"
-removeFile "vim-code-dark/.git/"
+removeFile "vim-code-dark/LICENSE.md" &
+removeFile "vim-code-dark/README.md" &
+removeFile "vim-code-dark/base16/" &
+removeFile "vim-code-dark/.git/" &
+removeFile "vim-code-dark/.github/" &
 
 # remove unused from vim-floaterm
-removeFile "vim-floaterm/test/"
-removeFile "vim-floaterm/README.md"
-removeFile "vim-floaterm/LICENSE"
-removeFile "vim-floaterm/doc/"
-removeFile "vim-floaterm/.git/"
+removeFile "vim-floaterm/test/" &
+removeFile "vim-floaterm/README.md" &
+removeFile "vim-floaterm/LICENSE" &
+removeFile "vim-floaterm/doc/" &
+removeFile "vim-floaterm/bin/" &
+removeFile "vim-floaterm/.git/" &
+removeFile "vim-floaterm/.gitignore/" &
+removeFile "vim-floaterm/.github/" &
 
 # remove unused from ale
-removeFile "ale/test/"
-removeFile "ale/rplugin/"
-removeFile "ale/LICENSE"
-removeFile "ale/README.md"
-removeFile "ale/run-tests"
-removeFile "ale/run-tests.bat"
-removeFile "ale/Dockerfile"
-removeFile "ale/supported-tools.md"
-removeFile "ale/test/"
-removeFile "ale/doc/"
-removeFile "ale/.git/"
+removeFile "ale/test/" &
+removeFile "ale/rplugin/" &
+removeFile "ale/LICENSE" &
+removeFile "ale/README.md" &
+removeFile "ale/run-tests" &
+removeFile "ale/run-tests.bat" &
+removeFile "ale/Dockerfile" &
+removeFile "ale/supported-tools.md" &
+removeFile "ale/test/" &
+removeFile "ale/doc/" &
+removeFile "ale/.git/" &
+removeFile "ale/.appveyor.yml" &
+removeFile "ale/.editorconfig" &
+removeFile "ale/.gitignore" &
+removeFile "ale/.vintrc.yaml" &
+removeFile "ale/.gitattributes" &
+removeFile "ale/.github" &
 
 # template
 # removeFile ""
 
-
-echo ""
-echo ""
-echo "    Config builded!!!"
-echo "    have a nice day)"
-echo ""
-echo ""
+#
+# echo ""
+# echo ""
+# echo "    Config builded!!!"
+# echo "    have a nice day)"
+# echo ""
+# echo ""
+#
