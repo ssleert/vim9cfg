eeef4336d29543eb790cc82c4a794b9f4eda796f
ssleert <semenilichevsky@gmail.com>
2022-08-29 01:57:25 +0300

    Update: nixos support added

--- src/vcb.sh
+++ src/vcb.sh
@@ -1,17 +1,17 @@
 installPlug() {
   cd $HOME/.vim/pack/default/start
-  /usr/bin/git clone --quiet "https://github.com/$1.git"
-  /usr/bin/echo "installed "$1
+  git clone --quiet "https://github.com/$1.git"
+  echo "installed "$1
 }
 
 removeFile() {
   cd $HOME/.vim/pack/default/start
-  /usr/bin/rm -rf $1
+  rm -rf $1
 }
 
-/usr/bin/rm -rf ~/.vim
-/usr/bin/mkdir -p ~/.vim/pack/default/start
+rm -rf ~/.vim
+mkdir -p ~/.vim/pack/default/start
 
-/usr/bin/cp staff/vimrc ~/.vim/vimrc
+cp staff/vimrc ~/.vim/vimrc
 
 source conf/setup.sh
